7e7d2d83c3cbeaaab91a9b66781c01bb6d781dd1
Elias Kupeczki <github@kunoxy.ch>
2023-02-18 13:51:28 +0100

    first commit

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWpQepz9V_piGaPUv53rBrsQEgW0_SG20",
+  authDomain: "modul152-e493c.firebaseapp.com",
+  projectId: "modul152-e493c",
+  storageBucket: "modul152-e493c.appspot.com",
+  messagingSenderId: "654396779624",
+  appId: "1:654396779624:web:ca9570dc6e87aea9189fdd"
+  
+};
+
+// Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const app = initializeApp(firebaseConfig);
+const perf = getPerformance(app);
---
+++ src/pages/Navigationbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <nav className="flex  w-full justify-end bg-gradient-to-r from-[#5F0A87] to-[#A4508B]"></nav>
+  );
+};
+export default header;
