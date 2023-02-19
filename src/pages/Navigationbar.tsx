5353cf5da878e75ac0d33036ddd8abcb02ba4345
Elias Kupeczki <github@kunoxy.ch>
2023-02-19 11:57:08 +0100

    switch to my pc

--- src/pages/Navigationbar.tsx
+++ src/pages/Navigationbar.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
+type Header = {
+  show: boolean;
+};
 const Header = () => {
   //path Animeation for x https://www.framer.com/motion/introduction/
   //for button to rotate from point of origin use this link https://tailwindcss.com/docs/transform-origin to rotate on click
@@ -9,11 +12,29 @@ const Header = () => {
   return (
     <>
       <div>
-        <div className="z-100  absolute top-5 right-5 flex h-10 w-10 justify-center rounded-full bg-blue-100">
-          <button onClick={() => toggleShow(!show)}>X</button>
-        </div>
+        <label className="swap-rotate swap absolute right-5 top-5 rounded-full bg-teal-100 p-1">
+          <input type="checkbox" onClick={() => toggleShow(!show)} />
+          <svg
+            className="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
 
-        {show && <Sidebar />}
+          <svg
+            className="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        </label>
+        {show && <Sidebar show={false} />}
       </div>
     </>
   );
--- src/pages/Sidebar.tsx
+++ src/pages/Sidebar.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-
-const Sidebar = () => {
+type SidebarProps = {
+  show: boolean;
+};
+const Sidebar = ({ show }: SidebarProps) => {
   return (
     <>
-      <div className="group:bg-blue-100 absolute right-0 top-0 -z-50 h-full w-1/4 bg-blue-900 duration-700">
+      <div
+        className={`group:bg-blue-100 absolute right-0 top-0 -z-50 h-full 
+      w-1/4 transform bg-teal-400 ease-in-out ${
+        show ? "bg-red-50" : "bg-blue-900"
+      }`}
+      >
         <div className="flex h-full flex-col justify-center gap-10">
-          <div className="pl-4 font-mono text-7xl font-extrabold">Home</div>
-          <div className="pl-4 font-mono text-7xl font-extrabold">Gallery</div>
-          <div className="pl-4 font-mono text-7xl font-extrabold">
-            Something
+          <div className="duration-30 pl-4 font-mono text-7xl font-extrabold text-stone-700 transition-all hover:translate-x-4 hover:underline">
+            Home
           </div>
-          <div className="pl-4 font-mono text-7xl font-extrabold">Contact</div>
+          <div className="duration-30 pl-4 font-mono text-7xl font-extrabold text-stone-700 transition-all hover:translate-x-4 hover:underline">
+            Login
+          </div>
+          <div className="duration-30 pl-4 font-mono text-7xl font-extrabold text-stone-700 transition-all hover:translate-x-4 hover:underline">
+            Signup
+          </div>
+          <div className="duration-30 pl-4 font-mono text-7xl font-extrabold text-stone-700 transition-all hover:translate-x-4 hover:underline">
+            Contact
+          </div>
         </div>
       </div>
     </>
