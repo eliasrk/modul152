5b6902cb708e846fb32db5d36cee43caf9940fd1
Elias Kupeczki <github@kunoxy.ch>
2023-02-20 16:33:25 +0100

    added lazyloading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { type NextPage } from "next";
 import Layout from "./Layout";
 import Navigationbar from "../components/Navigationbar";
+import { SetStateAction } from "react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Navigationbar></Navigationbar>
+      <Navigationbar
+        setIsUpload={function (value: SetStateAction<boolean>): void {
+          throw new Error("Function not implemented.");
+        }}
+        name={""}
+      ></Navigationbar>
       <Layout></Layout>
     </>
   );
